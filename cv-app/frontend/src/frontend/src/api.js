b7e5ee2fe1249e614d1f4eabd916f0d81cde3503
HARIS30 <hrsalfrsy30@gmail.com>
2025-03-07 16:08:26 +0700

    first commit

--- cv-app/frontend/src/frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5000";
-
-export const login = (email, password) => axios.post(`${API_URL}/login`, { email, password });
-export const saveCV = (data) => axios.post(`${API_URL}/save-cv`, data);
-export const getCV = () => axios.get(`${API_URL}/get-cv`);
